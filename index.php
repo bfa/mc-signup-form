99be02df930035288f003b2f977e4c8b5f5216ef
Brent Alexander <brent@bfa.me>
2016-03-05 14:16:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en-US"> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" lang="en-US"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en-US"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>MailChimp Signup Form w/ Cookies</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+</head>
+<body>
+
+<!-- HTML Body Goes Here -->
+<h1>MailChimp Signup Form w/ Cookies</h1>
+<p>Written by Brent Alexander</p>
+<p><a href="http://bfa.me" target="_blank">www.bfa.me</a></p>
+
+<!-- Scripts -->
+<script src="js/jquery-1.10.0.min.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+
+<?php include('parts/mc-signup-form.php'); ?>
+
+</body>
+</html>
